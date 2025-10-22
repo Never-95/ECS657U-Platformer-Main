c7455668f4a4e63e5c688751ed6f8a15d7575cb7
Sathavan18 <bt21583@qmul.ac.uk>
2025-10-22 13:50:07 +0100

    Added End Level Coin

--- Assets/Scripts/PerkCoin.cs
+++ Assets/Scripts/PerkCoin.cs
@@ -1,11 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class PerkCoin : MonoBehaviour
 {
-    public enum PerkType { SpeedBoost, DoubleJump, Invisibility }
+    public enum PerkType { SpeedBoost, DoubleJump, Invisibility, LevelEnd }
     public PerkType perkType;
     public float effectDuration = 5f;
     public float speedMultiplier = 2f;
+
+    [Header ("Level End Settings")]
+    public string nextSceneName = "";
+    public float levelEndDelay = 1f;
+    public TextMeshProUGUI levelCompleteText;
+    public string completionMessage = "Congratulations! Level Complete!";
 
     private void OnTriggerEnter(Collider other)
     {
@@ -26,10 +35,40 @@ public class PerkCoin : MonoBehaviour
                     case PerkType.Invisibility:
                         player.ActivateInvisibility(effectDuration);
                         break;
+                    case PerkType.LevelEnd:
+                        StartCoroutine(EndLevelProcess());
+                        return;
                 }
 
                 Destroy(gameObject, 0.1f);
             }
+        }
+    }
+
+    private System.Collections.IEnumerator EndLevelProcess()
+    {
+        Debug.Log("Level Complete!");
+        Destroy(gameObject);
+
+        if (levelCompleteText != null)
+        {
+            levelCompleteText.gameObject.SetActive(true);
+            levelCompleteText.text = completionMessage;
+        }
+
+        yield return new WaitForSeconds(levelEndDelay);
+
+        if (!string.IsNullOrEmpty(nextSceneName))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+        else
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            Application.Quit();
+            #endif
         }
     }
 }
